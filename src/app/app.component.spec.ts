345666517347b3f1ef6645e39f8f748576f6ff62
Matt Bernhardt <mjbernha@mit.edu>
2020-05-21 15:54:00 -0400

    Fix failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-bento'`, () => {
+  it(`should have as title 'Angular Bento'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-bento');
+    expect(app.title).toEqual('Angular Bento');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-bento app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Bento');
   });
 });
